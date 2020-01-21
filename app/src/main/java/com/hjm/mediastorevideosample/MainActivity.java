1f7707c118bb92021839e18d27f6581ee5577904
Hajime Shiozawa <shiozawa@ripplex.com>
2020-01-21 14:25:18 +0900

    replace String with MediaFIle class

--- app/src/main/java/com/hjm/mediastorevideosample/MainActivity.java
+++ app/src/main/java/com/hjm/mediastorevideosample/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +18,22 @@ import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+
+    private static class MediaFile {
+        public final String path;
+        public final String mimeType;
+
+        public MediaFile(String path, String mimeType) {
+            this.path = path;
+            this.mimeType = mimeType;
+        }
+
+        @Override
+        public String toString() {
+            return "path=" + path + ", " + "mimeType=" + mimeType;
+        }
+    }
 
     private static final int REQUEST_PERMISSION = 10;
 
@@ -33,31 +50,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Uri target = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                List<String> paths = getMediaStoreFiles(target);
+                List<MediaFile> files = getMediaStoreFiles(target);
                 StringBuilder builder = new StringBuilder(1024);
-                for (String path : paths) {
-                    builder.append(path).append("\n");
+                for (MediaFile file : files) {
+                    builder.append(file).append("\n");
                 }
                 textView.setText(builder.toString());
             }
         });
     }
 
-    private List<String> getMediaStoreFiles(Uri uri) {
+    private List<MediaFile> getMediaStoreFiles(Uri uri) {
         ContentResolver contentResolver = getContentResolver();
-        List<String> ret = new ArrayList<>();
+        List<MediaFile> ret = new ArrayList<>();
         try (Cursor cursor = contentResolver.query(uri,
                 null, null, null, null)) {
             if (cursor != null && cursor.moveToFirst()) {
                 do {
                     String path =
                             cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-                    ret.add(path);
+                    String mimeType =
+                            cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.MIME_TYPE));
+                    ret.add(new MediaFile(path, mimeType));
                 } while (cursor.moveToNext());
             }
         } catch (Exception e) {
-            ret.clear();
-            ret.add("error : " + e.getLocalizedMessage());
+            Log.e(TAG, e.toString());
             return ret;
         }
         return ret;
